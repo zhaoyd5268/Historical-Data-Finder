ced1b76e79220ca4f87dcd44c0e18474c8a36a51
zhaoyd5268 <zhaoyd5268@gmail.com>
2024-07-20 15:56:24 -0700

    initla
    initial commit

---
+++ Historical-Data-Finder/src/main/java/org/jointheleague/api/Historical/Data/Finder/Historical/Data/Finder/HistoricalDataFinderApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.Historical.Data.Finder.Historical.Data.Finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricalDataFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricalDataFinderApplication.class, args);
+	}
+
+}

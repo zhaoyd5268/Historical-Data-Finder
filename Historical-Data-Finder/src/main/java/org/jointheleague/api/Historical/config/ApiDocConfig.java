67a02a3179debee70f9db6b49f6bdadc86b1c5e0
zhaoyd5268 <zhaoyd5268@gmail.com>
2024-08-24 16:15:14 -0700

    Im Finished!!!!! On to studying for Oracle!!!! :):):)

--- Historical-Data-Finder/src/main/java/org/jointheleague/api/Historical/config/ApiDocConfig.java
+++ Historical-Data-Finder/src/main/java/org/jointheleague/api/Historical/config/ApiDocConfig.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 
 @Configuration
 public class ApiDocConfig {
-//asdfasdfasdf
+//asdfasdfasdfasefvafvafvafeb v
   /*  private ApiInfo apiInfo() {
         return new ApiInfo(
                 "Level 7 Historical Data Finder",
